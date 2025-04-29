b7920818be1d3ef70adac74c24103fa6ad6d579f
Ishmoth Ura Nuri <nuri@adfinix.com>
2025-04-29 18:43:28 +0600

    auth folder create1.0

---
+++ app/_lib/page.tsx
@@ -0,0 +1,9 @@
+export default function PrivatePage() {
+  return (
+    <div>
+      <h1>this is privet page</h1>
+    </div>
+  );
+}
+// privet page kaje e lage je amra ui logic theke kono kisu ke sepration korer jonno 
+//
